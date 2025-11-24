e18466035bbaeedd0429de6df6a82f722d107b99
Ashutosh Moharana <ashutoshmoharana00@gmail.com>
2025-11-24 14:04:21 +0530

    new look update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,52 +5,48 @@ import { BsTwitterX } from "react-icons/bs";
 import { LuPhoneCall } from "react-icons/lu";
 
 const SocialLinks = () => {
-  const socialLinks = useMemo(
-    () => [
-      {
-        name: "GitHub",
-        icon: <SiGithub color="var(--color-primary)" size={24} />,
-        url: "https://github.com/ashutosh-moharana",
-      },
-      {
-        name: "LinkedIn",
-        icon: <SiLinkedin color="var(--color-primary)" size={24} />,
-        url: "https://linkedin.com/in/ashutosh-moharana",
-      },
-      {
-        name: "Email",
-        icon: <HiOutlineMail color="var(--color-primary)" size={24} />,
-        url: "mailto:ashutoshmoharana00@gmail.com",
-      },
-      {
-        name: "Phone",
-        icon: <LuPhoneCall color="var(--color-primary)" size={24} />,
-        url: "tel:9937727738",
-      }
-    ],
-    []
-  );
+    const socialLinks = useMemo(
+        () => [
+            {
+                name: "GitHub",
+                icon: <SiGithub color="var(--color-primary)" size={24} />,
+                url: "https://github.com/ashutosh-moharana",
+            },
+            {
+                name: "LinkedIn",
+                icon: <SiLinkedin color="var(--color-primary)" size={24} />,
+                url: "https://linkedin.com/in/ashutosh-moharana",
+            },
+            {
+                name: "Email",
+                icon: <HiOutlineMail color="var(--color-primary)" size={24} />,
+                url: "mailto:ashutoshmoharana00@gmail.com",
+            },
+            {
+                name: "Phone",
+                icon: <LuPhoneCall color="var(--color-primary)" size={24} />,
+                url: "tel:9937727738",
+            }
+        ],
+        []
+    );
 
-  return (
-    <div className="flex flex-col gap-4 ">
-      {socialLinks.map((link, index) => (
-        <a
-      
-          key={index}
-          href={link.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="group mb-4 text-subtle flex items-center gap-3 px-8 py-3  bg-transparent border  hover:border-primary hover:scale-95  transition-all"
-         
-        >
-          {link.icon}
-          
-          {link.name}
-          
-        </a>
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-4 ">
+            {socialLinks.map((link, index) => (
+                <a
+                    key={index}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="group mb-4 text-subtle flex items-center gap-3 px-8 py-3  bg-transparent border  hover:border-primary hover:scale-95  transition-all"
+                >
+                    {link.icon}
+                    {link.name}
+                </a>
+            ))}
+        </div>
+    );
 };
 
 export default SocialLinks;
